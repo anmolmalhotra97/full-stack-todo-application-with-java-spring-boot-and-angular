e01f01acd10770bf6d1d40aeea341eb57f0f0e17
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-06-03 16:05:15 -0400

    created a basic todo list and formatted its structure using class

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
+
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id: 1, description: 'Learn Angular' },
-    { id: 2, description: 'Learn TS' },
-    { id: 3, description: 'Learn GO Lang' },
+    new Todo(1, 'Learn Angular', false, new Date()),
+    new Todo(2, 'Learn TS', true, new Date()),
+    new Todo(3, 'Learn Go Lang', false, new Date()),
   ]
 
   constructor() { }
@@ -18,4 +21,14 @@ export class ListTodosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+//DONT PUT this RIGHT BELOW @Component btw @Component and Component Class ---> things would break
+//anywhere else is fine
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date) { }
 }
