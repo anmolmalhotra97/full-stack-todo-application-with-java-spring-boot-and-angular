fd559e459d2aab14218613cbae45da484545c967
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-06-03 02:11:52 -0400

    explored with differences and similarities between TS, Java, and JS

--- frontend/todo/src/app/welcome/welcome.component.ts
+++ frontend/todo/src/app/welcome/welcome.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
 export class WelcomeComponent implements OnInit {
 
+  message : string = 'Some Welcome Message'
   constructor() { }
 
   ngOnInit(): void {
+    //COMPILATION ERROR
+    //this.message = 5
+    console.log(this.message)
   }
 
 }
