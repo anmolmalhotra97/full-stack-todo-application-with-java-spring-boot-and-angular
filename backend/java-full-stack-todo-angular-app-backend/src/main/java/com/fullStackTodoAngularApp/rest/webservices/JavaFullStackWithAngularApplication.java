8fc958668a3cfd797e0d50f6dcc3c7b9883f3ffd
Anmol Malhotra <anmolmalhotra97@gmail.com>
2022-06-03 20:49:13 -0400

    started working on rest webservices for spring backend

---
+++ backend/java-full-stack-todo-angular-app-backend/src/main/java/com/fullStackTodoAngularApp/rest/webservices/JavaFullStackWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.fullStackTodoAngularApp.rest.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFullStackWithAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFullStackWithAngularApplication.class, args);
+    }
+
+}
---
+++ backend/java-full-stack-todo-angular-app-backend/src/test/java/com/fullStackTodoAngularApp/rest/webservices/JavaFullStackWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStackTodoAngularApp.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFullStackWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
